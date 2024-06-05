de64e85ce10a41f5ddaca5c8f921dcbbc54b09ce
Dobataa <dobatabg04@gmail.com>
2024-06-05 14:08:36 +0300

    display fetched rates

--- controllers/ratesController.js
+++ controllers/ratesController.js
@@ -4,8 +4,16 @@ const getRates = require('../api/api');
 const router = Router();
 
 router.get('/', async (req, res) => {
-    const rates = await getRates();
-    res.json(rates);
+    try {
+        const data = await getRates();
+        let names = Object.keys(data.rates);
+        let rates = Object.values(data.rates);
+
+        res.status(200).render('rates', {title: 'Rates', names, rates});
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+    
 });
 
 module.exports = router;
--- routes.js
+++ routes.js
@@ -5,5 +5,8 @@ const router = Router();
 const ratesController = require('./controllers/ratesController');
 
 router.use('/rates', ratesController);
+router.get('*', (req, res) => {
+    res.render('404');
+}) 
 
 module.exports = router;
